aafa25d8a96db3538488544287d3f88078eeb9e4
ArnobXSabit <arnobxsabit@gmail.com>
2025-10-08 21:51:40 +0600

    Use env variables for DB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,18 @@
-// backend/config/db.js
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '1613751326',
-  database: 'PureWaveDB',
+  host: process.env.DB_HOST,        // Railway host
+  user: process.env.DB_USER,        // root
+  password: process.env.DB_PASSWORD, // your password
+  database: process.env.DB_NAME,     // railway
+  port: process.env.DB_PORT,         // 56981
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-console.log("✅ MySQL2 connected");
+pool.getConnection()
+  .then(() => console.log("✅ MySQL2 connected"))
+  .catch(err => console.error("❌ MySQL2 connection error:", err));
 
 module.exports = pool;
